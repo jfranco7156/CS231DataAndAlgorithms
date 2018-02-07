b9a6f8effbc7f2acdfd924c3d7062787e29df7e5
jfranco7156 <jfranco7156@bths.edu>
2018-02-07 12:03:30 -0500

    Began Lab section of project

--- CS231/src/project1/Shuffle.java
+++ CS231/src/project1/Shuffle.java
@@ -1,10 +1,22 @@
 package project1;
 
+/**
+ * File: Shuffle.java
+ * Author: FirstName LastName
+ * Date: MM/DD/YYYY
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Shuffle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList arrayL = new ArrayList();
+		Random rand = new Random();
+		for(int i=0; i<10;i++) {
+			arrayL.add(rand.nextInt(100));
+		}
 	}
 
 }
