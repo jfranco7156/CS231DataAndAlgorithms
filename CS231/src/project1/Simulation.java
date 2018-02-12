9092fc5ddaa3b6c9fc58e4ab963dfa5f24943fe8
jfranco7156 <jfranco7156@bths.edu>
2018-02-12 16:34:49 -0500

    Added Simulation class to run Blackjack class

---
+++ CS231/src/project1/Simulation.java
@@ -0,0 +1,30 @@
+package project1;
+
+public class Simulation {
+
+	public static void main(String[] args) {
+		int playerWins = 0;
+		int dealerWins = 0;
+		int ties = 0;
+		Blackjack game = new Blackjack();
+		for(int i=0; i<1000; i++) {
+			if(game.getD().deck.size()<20) {
+				game.reset(true);
+			}
+			int winner = game.playRound();
+			if(winner==0) {
+				ties++;
+			}
+			else if(winner==1) {
+				playerWins++;
+			}
+			else {
+				dealerWins++;
+			}
+		}
+		System.out.println("The player has won " + playerWins+" times. A percentage of "+(playerWins/10));
+		System.out.println("The dealer has won "+ dealerWins+" times. A percentage of "+(dealerWins/10));
+		System.out.println("There has been "+ties+" pushes.");
+	}
+
+}
